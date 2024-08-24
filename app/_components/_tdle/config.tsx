434296ce8cc2772efb3b0b6216c8cf556897c962
Praneeth Shetty <praneethshetty10@gmail.com>
2024-08-24 11:59:28 +0530

    adding wallet connect and user points

--- app/_components/_tdle/config.tsx
+++ app/_components/_tdle/config.tsx
@@ -1,13 +1,32 @@
+import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
 import { http, createConfig } from 'wagmi'
-import { base, mainnet, arbitrumSepolia } from 'wagmi/chains'
-import { injected } from 'wagmi/connectors'
+import { arbitrumSepolia } from 'wagmi/chains'
+import { walletConnect } from 'wagmi/connectors'
 
+export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID as string
+// export const config = createConfig({
+//     chains: [arbitrumSepolia],
+//     connectors: [walletConnect({
+//         projectId: '4c0f2636de227a57ee1344455832fbf7',
+//     }),],
+//     transports: {
+//         [mainnet.id]: http(),
+//         [base.id]: http(),
+//         [arbitrumSepolia.id]: http()
+//     },
+// })
 
-export const config = createConfig({
-    chains: [mainnet, base, arbitrumSepolia],
-    transports: {
-        [mainnet.id]: http(),
-        [base.id]: http(),
-        [arbitrumSepolia.id]: http()
-    },
+export const metadata = {
+    name: 'TDLE',
+    description: 'Telegram mini app worDLE',
+    url: 'https://tvia.vercel.app',
+    icons: ['https://avatars.githubusercontent.com/u/37784886']
+}
+
+const chains = [arbitrumSepolia] as const
+export const config = defaultWagmiConfig({
+    chains,
+    projectId,
+    metadata,
+    ssr: true,
 })
--- app/_components/_tdle/navBar.tsx
+++ app/_components/_tdle/navBar.tsx
@@ -1,24 +1,43 @@
-import { useAccount, useConnect } from 'wagmi'
+import { useAccount, useConnect, useReadContract } from 'wagmi'
+import { useTdleContext } from './tdleContext'
 
 export default function NavBar() {
 
-    const { isConnected, connector } = useAccount()
+    const { isConnected, connector, address: accountAddress } = useAccount()
     const { connectors, connect } = useConnect()
+    const { CONTRACT_ABI, CONTRACT_ADDRESS } = useTdleContext()
+
+    const { data, failureReason, isSuccess } = useReadContract({
+        abi: CONTRACT_ABI as any,
+        address: CONTRACT_ADDRESS as any,
+        functionName: 'balanceOf',
+        args: [accountAddress as any]
+    })
 
     return <div className='flex items-center justify-center mx-5 gap-10'>
         <h1 className="text-5xl font-semibold p-2 text-center">Tdle</h1>
         {isConnected ?
-            <button onClick={() => connector?.disconnect()} className="my-2 p-2 border-2 rounded-full font-semibold">
-                Disconnect Wallet
-            </button> : <>{connectors.map((connector) => (
-                <button
-                    key={connector.uid}
-                    onClick={() => connect({ connector })}
-                    className="my-2 p-2 border-2 rounded-full font-semibold "
-                >
-                    Connect Wallet
-                </button>
-            ))}</>}
+            // <button onClick={() => connector?.disconnect()} className="my-2 p-2 border-2 rounded-full font-semibold">
+            //     Disconnect Wallet
+            // </button> 
+            <>
+                <w3m-account-button />
+                <div className="p-1.5 border-2 rounded-full font-semibold">{data?.toString()} TDLE</div>
+            </>
+            :
+            // <>{connectors.map((connector) => (
+            //     <button
+            //         key={connector.uid}
+            //         onClick={() => connect({ connector })}
+            //         className="my-2 p-2 border-2 rounded-full font-semibold "
+            //     >
+            //         Connect Wallet
+            //     </button>
+            // ))}</>
+
+            <w3m-button />
+
+        }
 
     </div>
 }
